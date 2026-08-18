098d547b8fe3a97538014b5b93d739e4ad73a235
shrishtisinha1001-lab <shrishtisinha1001@gmail.com>
2026-08-18 12:10:31 +0530

    Add: palindrome-linked-list - submission-3

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-3.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if(head == null)
+        {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        ListNode current = head;
+
+        while(current!=null)
+        {
+            list.add(current.val);
+            //move the curr pointer to the next node
+            current = current.next;
+
+        }
+        int left=0;
+        int right= list.size()-1;
+        while(left<right)
+        {
+            if(list.get(left)!=list.get(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+        
+    }
+}
